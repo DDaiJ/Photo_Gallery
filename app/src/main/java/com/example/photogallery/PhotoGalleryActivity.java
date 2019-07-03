f4735ca7db8278d8aff29b147ab16ee1354fa296
Dai, Jessica <daij7@uw.edu>
2019-07-03 14:59:40 -0700

    first

---
+++ app/src/main/java/com/example/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,12 @@
+package com.example.photogallery;
+
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+    }
+}
